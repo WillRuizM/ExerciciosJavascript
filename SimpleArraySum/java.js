d3c6cf3373024346beb888245d342eca65608b5e
WillRuizM <141746428+WillRuizM@users.noreply.github.com>
2024-03-25 23:58:55 -0300

    Add files via upload

---
+++ SimpleArraySum/java.js
@@ -0,0 +1,10 @@
+  
+    function simpleArraySum(ar) {
+        let soma = 0;
+        for(var i = 0; i < ar.length; i++){
+            soma = soma + ar [i]
+        }
+        return soma;
+    
+    }
+
